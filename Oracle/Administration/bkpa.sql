632eff8c364aeeecc493e1866ef72d195f2019ff
ralfsilvadba <ralfsilvadba@gmail.com>
2019-12-13 09:08:38 -0300

    Create bkpa.sql

---
+++ Oracle/Administration/bkpa.sql
@@ -0,0 +1,16 @@
+set lines 1000
+
+select * from (
+select session_recid,
+        session_stamp,
+        --session_key, input_type, status,
+        status,
+        to_char(start_time,'dd/mm/yyyy hh24:mi') start_time,
+        to_char(end_time,'dd/mm/yyyy hh24:mi') end_time,
+        floor(input_bytes/1024/1024) size_mb,
+        trunc(elapsed_seconds/60) min,
+        input_type
+from v$rman_backup_job_details
+--where input_type in ('DB FULL','DB INCR')
+where input_type = 'ARCHIVELOG'
+order by session_recid desc) where rownum <= DECODE(&1,10,10,100);
